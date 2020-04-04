e9bf932a527edcd1961c6152cb4ca84d6c9de402
SinonWang <2606233967@qq.com>
2020-04-04 14:19:21 +0800

    优化目录结构

---
+++ Example/K210_SD_Card/.vscode/cmake-server.bat
@@ -0,0 +1,9 @@
+cd "J:/Kendryte_IDE/KendryteIDE/LocalPackage/cmake/bin"
+SET Path=j:\Kendryte_IDE\KendryteIDE\bin;J:\Kendryte_IDE\KendryteIDE\LocalPackage\toolchain\bin;J:\Kendryte_IDE\KendryteIDE\LocalPackage\cmake\bin;J:\Kendryte_IDE\KendryteIDE\LocalPackage\jlink;J:\Kendryte_IDE\KendryteIDE\LocalPackage\clang-format\bin;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0;C:\WINDOWS\System32\OpenSSH
+SET PYTHONHOME=J:/Kendryte_IDE/KendryteIDE/LocalPackage/python2library
+SET PYTHONPATH=J:/Kendryte_IDE/KendryteIDE/LocalPackage/python2library;J:/Kendryte_IDE/KendryteIDE/LocalPackage/toolchain/share/gdb/python
+SET PYTHONDONTWRITEBYTECODE=yes
+SET CMAKE_MAKE_PROGRAM=make
+SET KENDRYTE_IDE=yes
+SET LANG=en_US.utf-8
+"J:/Kendryte_IDE/KendryteIDE/LocalPackage/cmake/bin/cmake.exe" -E server --experimental --pipe=\\?\pipe\kide.cmake_server_pipe.1585980668971
---
+++ Example/K210_SD_Card/build/CMakeFiles/K210_SD_Card.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/K210_SD_Card.dir/src/main.c.obj"
+  "K210_SD_Card.pdb"
+  "K210_SD_Card"
+  "K210_SD_Card.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/K210_SD_Card.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
